cf2439d211ace67b7db7b511927cc80ef36cd361
kabirodavies <dnkabiro@gmail.com>
2023-05-16 14:43:37 +0300

    first commit

---
+++ gatsby-config.ts
@@ -0,0 +1,54 @@
+import type { GatsbyConfig, PluginRef } from "gatsby"
+import "dotenv/config"
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
+    siteTitle: `Davies Portifolio`,
+    // siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    // siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    // siteUrl: `https://cara.lekoarts.de`,
+    // siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    // siteImage: `/banner.jpg`,
+    siteLanguage: `en`,
+    author: `@lekoarts_de`,
+  },
+  trailingSlash: `never`,
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Cara - @lekoarts/gatsby-theme-cara`,
+        short_name: `Cara`,
+        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        start_url: `/`,
+        background_color: `#141821`,
+        icon: "static/favicon.ico",
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#f6ad55`,
+        display: `standalone`,
+      },
+    },
+    // You can remove this plugin if you don't need it
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-statoscope`,
+      options: {
+        saveReportTo: `${__dirname}/public/.statoscope/_bundle.html`,
+        saveStatsTo: `${__dirname}/public/.statoscope/_stats.json`,
+        open: false,
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>,
+}
+
+export default config
